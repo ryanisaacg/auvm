f49c5f9e3745fd802b6bbdac4a9eb60fe7411426
Ryan Goldstein <ryanisaacg@gmail.com>
2016-12-13 07:59:28 -0500

    Continued working on initializing the machine's hardware

--- machine/machine.h
+++ machine/machine.h
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define RAM_SIZE 1048576
+
 typedef unsigned char ubyte;
 typedef char byte;
 
 FILE *disk;
 ubyte *ram, *disk_buffer;
-size_t ram_size, disk_size;
+size_t disk_size;
 int registers[8];
