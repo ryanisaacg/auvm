8b1a89cac40f7ee78b95a7eee9aa7865a27b8389
EpicPieGuy <ryanisaacg@gmail.com>
2016-12-13 21:48:17 -0500

    Started implementing MOV and WTO to get a sample program created

--- machine/machine.c
+++ machine/machine.c
@@ -1,5 +1,6 @@
 #include "machine.h"
 
+#include "io.h"
 #include "instructions.h"
 #include <stdbool.h>
 #include <string.h>
@@ -41,38 +42,21 @@ void load_disk() {
 	if(disk_buffer == NULL) {
 		fprintf(stderr, "Memory allocation failed in load_disk\n");
 	}
-	disk_size = 1024;
-	ubyte disk_copy[1024];
-	size_t disk_position = 0;
-	while(!feof(disk)) {
-		size_t disk_read = fread(disk_copy, 1, 1024, disk);
-		if(disk_position + disk_read < disk_size) {
-			memcpy(disk_buffer + disk_position, disk_copy, 1024);
-		} else {
-			disk_buffer = realloc(disk_buffer, disk_size * 2);
-			if(disk_buffer == NULL) {
-				fprintf(stderr, "Memory reallocation failed in load_disk\n");
-			}
-			disk_size *= 2;
-			memcpy(disk_buffer + disk_position, disk_copy, 1024);
-		}
-		disk_position += disk_read;
-	}
-	disk_size = disk_position;
+	disk_buffer = read_file(disk, &disk_size);
+	fseek(disk, 0, SEEK_SET);
 }
 
 void write_disk() {
 	fwrite(disk_buffer, 1, disk_size, disk);
 }
 
-int get_value(ubyte *instruction) {
+number get_value(ubyte *instruction) {
 	ubyte type = instruction[0];
 	number value, pointer;
 	switch(type) {
 		case REGISTER:
 			memcpy(&value, instruction + 1, 4);
 			return registers[value];
-			break;
 		case REGISTER_VALUE:
 		case POINTER:
 			memcpy(&pointer, instruction + 1, 4);
@@ -81,6 +65,9 @@ int get_value(ubyte *instruction) {
 		case CONSTANT:
 			memcpy(&value, instruction + 1, 4);
 			return value;
+		default:
+			fprintf(stderr, "Failed switched statement in get_value");
+			exit(-1);
 	}
 }
 
@@ -119,4 +106,18 @@ void execute_bytecode(ubyte *data) {
 void execute_statement(ubyte *data, size_t position, size_t *new_position, bool *keep_going) {
 	data += position;
 	ubyte command = data[0];
+	ubyte *arguments = data + 1;
+	*keep_going = true;
+	switch(command) {
+		case MOV: {
+			number source = get_value(arguments);
+			set_value(arguments + 5, source);
+			*new_position = position + 11;
+		} break;
+		case WTO: {
+			number source = get_value(arguments);
+			putc(source, stdout);
+			*new_position = position + 5;
+		} break;
+	}
 }
