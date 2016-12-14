451723b66382520a4844355cd8d00a481f54dd3d
ryanisaacg <ryan@ryanisaacg.com>
2016-12-13 21:17:20 -0500

    Added the number typedef because all numbers are int32

--- machine/machine.h
+++ machine/machine.h
@@ -1,14 +1,20 @@
 #pragma once
 
 #include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
 
 #define RAM_SIZE 1048576
+#define REGISTER 0
+#define REGISTER_VALUE 1
+#define POINTER 2
+#define CONSTANT 3
 
 typedef unsigned char ubyte;
 typedef char byte;
+typedef int32_t number;
 
 FILE *disk;
 ubyte *ram, *disk_buffer;
 size_t disk_size;
-int registers[8];
+number registers[8];
