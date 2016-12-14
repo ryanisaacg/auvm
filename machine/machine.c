17d8eb990fca25799125826f901c4c085792511d
Ryan Goldstein <ryanisaacg@gmail.com>
2016-12-13 21:17:42 -0500

    Added functions to handle getting and setting a variety of memory types

--- machine/machine.c
+++ machine/machine.c
@@ -1,13 +1,16 @@
 #include "machine.h"
 
 #include "instructions.h"
+#include <stdbool.h>
 #include <string.h>
 
 void initialize_hardware();
 void load_disk();
 void write_disk();
-void execute_bytecode(ubyte *data, size_t length);
-size_t execute_statement(ubyte *data, size_t length, size_t position);
+void execute_bytecode(ubyte *data);
+void execute_statement(ubyte *data, size_t position, size_t *new_position, bool *keep_going);
+number get_value(ubyte *instruction);
+void set_value(ubyte *instruction, number value);
 
 int main() {
 	initialize_hardware();
@@ -62,3 +65,58 @@ void write_disk() {
 	fwrite(disk_buffer, 1, disk_size, disk);
 }
 
+int get_value(ubyte *instruction) {
+	ubyte type = instruction[0];
+	number value, pointer;
+	switch(type) {
+		case REGISTER:
+			memcpy(&value, instruction + 1, 4);
+			return registers[value];
+			break;
+		case REGISTER_VALUE:
+		case POINTER:
+			memcpy(&pointer, instruction + 1, 4);
+			memcpy(&value, ram + pointer, 4);
+			return value;
+		case CONSTANT:
+			memcpy(&value, instruction + 1, 4);
+			return value;
+	}
+}
+
+void set_value(ubyte *instruction, int value) {
+	ubyte type = instruction[0];
+	number index, pointer;
+	switch(type) {
+		case REGISTER:
+			memcpy(&index, instruction + 1, 4);
+			registers[index] = value;
+			break;
+		case REGISTER_VALUE:
+			memcpy(&index, instruction + 1, 4);
+			pointer = registers[index];
+			memcpy(ram + pointer, &value, 4);
+			break;
+		case POINTER:
+			memcpy(&pointer, instruction + 1, 4);
+			memcpy(ram + pointer, &value, 4);
+			break;
+		case CONSTANT:
+			fprintf(stderr, "Cannot set the value of a constant.\n");
+			exit(-1);
+			break;
+	}
+}
+
+void execute_bytecode(ubyte *data) {
+	size_t i = 0;
+	bool keep_going = true;
+	while(keep_going) {
+		execute_statement(data, i, &i, &keep_going);
+	}
+}
+
+void execute_statement(ubyte *data, size_t position, size_t *new_position, bool *keep_going) {
+	data += position;
+	ubyte command = data[0];
+}
