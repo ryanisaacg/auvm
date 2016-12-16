8f554ebeabb63d111746fb5b3b19829cf04700d9
ryanisaacg <ryan@ryanisaacg.com>
2016-12-16 09:09:57 -0500

    Added AST nodes to the program

---
+++ compiler/ast.h
@@ -0,0 +1,23 @@
+#pragma once
+
+struct Node;
+typedef struct Node Node;
+
+typedef union {
+	char *sval;
+	int ival;
+} NodeData;
+
+typedef enum { WORD_NODE, STRING_NODE, NUMBER_NODE, NIL_NODE} NodeType;
+
+struct Node {
+	NodeData data;
+	NodeType type;
+	Node *child, *next;
+};
+Node *node_new_nil();
+Node *node_new_str(char *data, NodeType type);
+Node *node_new_int(int data, NodeType type);
+Node *node_new(NodeData data, NodeType type);
+void node_add_child(Node *parent, Node *child);
+void node_print(Node *root);
