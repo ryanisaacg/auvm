c00a4ddb89c959dd36d295260377cfba44884c31
Ryan Goldstein <ryanisaacg@gmail.com>
2016-12-16 16:09:16 -0500

    Modified the table to work better

--- compiler/table.c
+++ compiler/table.c
@@ -10,23 +10,18 @@ Table *table_new(Table *parent) {
 	new->length = 0;
 	return new;
 }
-void table_add(Table *tbl, char *name, int value) {
+void table_add(Table *tbl, char *name) {
 	tbl->names[tbl->length] = name;
-	tbl->values[tbl->length] = value;
 	tbl->length++;
 	if(tbl->length > 1024) {
 		fputs("Added too many entries to a single table.", stderr);
 	}
 }
-int *table_get(Table *tbl, char *name) {
+int table_get(Table *tbl, char *name) {
 	for(size_t i = 0; i < tbl->length; i++) {
 		if(strcmp(tbl->names[i], name) == 0) {
-			return tbl->values + i;
+			return i;
 		}
 	}
-	if(tbl->parent != NULL) {
-		return table_get(tbl, name);
-	} else {
-		return NULL;
-	}
+	return -1;
 }
