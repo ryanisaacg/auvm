fe7b70ceba33a92adc0b6537dc339fa978cdf5d5
EpicPieGuy <ryanisaacg@gmail.com>
2016-12-17 17:54:12 -0500

    Added a function table implementation to store functions as the compiler runs

--- compiler/table.h
+++ compiler/table.h
@@ -15,3 +15,15 @@ struct Table {
 Table *table_new(Table *parent);
 void table_add(Table *tbl, char *name);
 int table_get(Table *tbl, char *name);
+
+struct FunctionTable;
+typedef struct FunctionTable FunctionTable;
+
+struct FunctionTable {
+	char *names[1024];
+	int labels[1024];
+	size_t length;
+};
+FunctionTable *func_table_new();
+void func_table_add(FunctionTable *tbl, char *name, int label);
+int func_table_get(FunctionTable *tbl, char *name);
