6f2f2611a023ed74f63dc7c71243fa1540b82d92
Ryan Goldstein <ryanisaacg@gmail.com>
2017-01-30 08:19:09 -0500

    Added a definition of Intermediate Representation

---
+++ compiler/ir.c
@@ -0,0 +1,107 @@
+#include "ir.h"
+
+#include "emitter.h"
+#include <stdlib.h>
+
+#define MOVE_NODE current->next = malloc(sizeof(IrNode)); current = current->next;
+#define PARAMS(n) malloc(sizeof(void*) * (n))
+
+IrNode *root, *current;
+
+void ir_start_file() {
+	root = malloc(sizeof(IrNode));
+	current = root;
+}
+
+void ir_end_file(FILE *out_stream) {
+	ir_emit(root, out_stream);
+}
+
+void ir_inline(char *contents) {
+	current->type = ASM;
+	current->params = PARAMS(1);
+	current->params[0] = contents;
+	MOVE_NODE;
+}
+
+void ir_new_var(char *name) {
+	current->type = VAR_NEW;
+	current->params = PARAMS(1);
+	current->params[0] = name;
+	MOVE_NODE;
+}
+
+void ir_get_var(char *name, int reg) {
+	current->type = VAR_NEW;
+	current->params = PARAMS(1);
+	current->params[0] = malloc(sizeof(int));
+	int *box = current->params[0];
+	*box = reg;
+	MOVE_NODE;
+}
+
+void ir_set_var(char *name, NodeData data, NodeType type) {
+
+}
+
+void ir_start_fun(char *name, char **args) {
+	current->type = DEF_FUN;
+	current->params = PARAMS(2);
+	current->params[0] = name;
+	current->params[1] = args;
+	MOVE_NODE;
+}
+
+void ir_end_fun() {
+	current->type = END_FUN;
+	MOVE_NODE;
+}
+
+void ir_call_fun(char *name, NodeData *data, NodeType *type, size_t num_args) {
+	current->type = CALL_FUN;
+	current->params = PARAMS(4);
+	current->params = (void*[]){ name, data, type, malloc(sizeof(size_t)) };
+	size_t *arg = current->params[3];
+	*arg = num_args;
+	MOVE_NODE;
+}
+
+void ir_return_fun(NodeData data, NodeType type) {
+	current->type = RETURN_FUN;
+	current->params = PARAMS(2);
+	NodeData *d = current->params[0] = malloc(sizeof(NodeData));
+	*d = data;
+	NodeType *t = current->params[1] = malloc(sizeof(NodeType));
+	*t = type;
+	MOVE_NODE;
+}
+
+void ir_start_main() {
+	current->type = MAIN;
+	MOVE_NODE;
+}
+
+void ir_end_main() {
+	current->type = END_MAIN;
+	MOVE_NODE;
+}
+
+void ir_if_start() {
+	current->type = IF_START;
+	MOVE_NODE;
+}
+
+void ir_while_start() {
+	current->type = WHILE_START;
+	MOVE_NODE;
+}
+
+void ir_cond_body_start() {
+	current->type = COND_BODY_START;
+	MOVE_NODE;
+}
+
+void ir_cond_body_end() {
+	current->type = COND_BODY_END;
+	MOVE_NODE;
+}
---
+++ compiler/ir.h
@@ -0,0 +1,43 @@
+#pragma once
+
+/*
+ * Intermediate Representation
+ * Converts from the user-friendly Abstract Syntax Tree into a stream format
+ * Adds the idea of structure to ASM concepts but is otherwise fairly low-level
+ * Generate with ir_ functions
+ * Emitted by the behavior defined in emitter.c
+ */
+
+#include <stddef.h>
+
+#include "ast.h"
+
+typedef enum { ASM, VAR_NEW, VAR_GET, VAR_SET, DEF_FUN, END_FUN, CALL_FUN, RETURN_FUN, MAIN, END_MAIN, IF_START, WHILE_START, COND_BODY_START, COND_BODY_END } IrType;
+
+typedef struct IrNode {
+	IrType type;
+	void **params;
+	struct IrNode *next;
+} IrNode;
+
+//All ir functions must be between ir_start_file and ir_end_file
+void ir_start_file();
+void ir_end_file(FILE *out_stream);
+//Pushes an asm command to the IR stream
+//char is the modifier for the ASM data type
+//null ('\0') character to terminate arguments
+void ir_inline(char *contents);
+void ir_new_var(char *name);
+void ir_get_var(char *name, int reg);
+void ir_set_var(char *name, NodeData data, NodeType type);
+//Null-terminate the arguments
+void ir_start_fun(char *name, char **args);
+void ir_end_fun();
+void ir_call_fun(char *name, NodeData *data, NodeType *type, size_t num_args);
+void ir_return_fun(NodeData data, NodeType type);
+void ir_start_main();
+void ir_end_main();
+void ir_if_start();
+void ir_while_start();
+void ir_cond_body_start();
+void ir_cond_body_end();
