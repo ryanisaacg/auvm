08c1f048d358fe22763344d1b5fee69f61090bd3
Ryan Goldstein <ryanisaacg@gmail.com>
2017-02-10 10:11:21 -0500

    Switched the ast part of the parser to emit ir instead of asm

--- compiler/ast.c
+++ compiler/ast.c
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "ir.h"
 #include "table.h"
 
 Node *node_new_nil() {
@@ -87,238 +89,100 @@ char *node_tostring(Node *node) {
 	return sval;
 }
 
-static inline void node_output_asm(Node *root, FILE *stream, char *command) {
+static void node_to_output(Node *root) {
 	char *sval = root->data.sval;
-	if(strcmp(sval, command) == 0) {
-		fprintf(stream, "%s ", command);
-		for(Node *child = root->child; child != NULL; child = child->next) {
-			fprintf(stream, "%s ", node_tostring(child));
-		}
-	}
-}
-
-#define NODE_MIRROR(str) if(strcmp(sval, str) == 0) { fputs(str, stream); return true; }
-
-static bool node_asm_literal(char *sval, FILE *stream) {
-	NODE_MIRROR("mov");
-	NODE_MIRROR("add");
-	NODE_MIRROR("sub");
-	NODE_MIRROR("mul");
-	NODE_MIRROR("div");
-	NODE_MIRROR("mod");
-	NODE_MIRROR("rfi");
-	NODE_MIRROR("wto");
-	NODE_MIRROR("cmp");
-	NODE_MIRROR("brn");
-	NODE_MIRROR("pfs");
-	NODE_MIRROR("pos");
-	NODE_MIRROR("and");
-	NODE_MIRROR("ior");
-	NODE_MIRROR("xor");
-	NODE_MIRROR("rhd");
-	NODE_MIRROR("whd");
-	NODE_MIRROR("end");
-	NODE_MIRROR("lbl");
-	NODE_MIRROR("gtb");
-	NODE_MIRROR("gcb");
-	return false;
-}
-
-FunctionTable *functions = NULL;
-Table *global_table = NULL;
-int label = 1;
-
-static void call_func(char *funcname, FILE *stream) {
-	//Get the current amount of stacks
-	//Add 1 to it
-	//Multiply it by the stack size (1024 in decimal, 400 in hex)
-	//Add 1 to it
-	//Get the current byte the CPU is looking at
-	//Add 36 to the value (24 in hex) to move past the instruction when returning
-	//Move that value into the first byte of the new stack
-	//Jump to the appropriate label for the function
-	fputs(	"mov %0 R0\n"
-			"add R0 =1 R0\n"
-			"mov R0 %0\n"
-			"mul R0 =1024 R0\n"
-			"add R0 =1 R0\n"
-			"gcb R1\n"
-			"add =43 R1 R1\n"
-			"mov R1 R$0\n", stream);
-	fprintf(stream, "brn =%d", func_table_get(functions, funcname));
-}
-
-static void func_return(FILE *stream) {
-	//Get the current amount of stacks
-	//Multiply it by the stack size (1024 in decimal, 400 in hex)
-	//Add 1 to it
-	//Subtract one from the amount of stacks
-	//Store it as the number of stacks
-	//Get the byte indicated by the first value in the stack
-	//Jump there
-	fputs(	"mov %0 R0\n"
-			"mul R0 =1024 R1\n"
-			"add R1 =1 R1\n"
-			"sub =1 R0 R0\n"
-			"mov R0 %0\n"
-			"gtb R$1", stream);
-}
-
-//TODO: var_new doesn't work
-static void var_new(Node *root, Table *table, FILE *stream) {
-	char *varname = root->child->data.sval;
-	fputs(	"; Create a new variable\n"
-			"mov %0 R0\n"
-			"mul R0 =1024\n"
-			"add R0 =5 R0\n"
-			"add R0 R$0 R1\n", stream);
-	Node *value = root->child->next;
-	if(value->type == WORD_NODE) {
-		int position = table_get(table, value->data.sval);
-		fprintf(stream, "add R0 =%d R2\n", position);
-		fputs("mov R$2 R$1\n", stream);
-	} else if(value->type == NUMBER_NODE) {
-		int number = value->data.ival;
-		fprintf(stream, "mov =%d R$1\n", number);
-	}
-	fputs(	"mov R$0 R3\n"
-			"mov R3 R$0\n"
-			"; End variable creation", stream);
-	table_add(table, varname);
-}
-
-static void var_get(Node *root, Table *table, FILE *stream) {
-	char *varname = root->child->data.sval;
-	int register_index = root->child->next->data.ival;
-	int offset = table_get(table, varname);
-	fputs(			"; Get the value of a variable into a register\n"
-					"mov %0 R0\n"
-					"mul R0 =1024\n"
-					"add R0 =5 R0\n", stream);
-	fprintf(stream, "add R0 =%d R0\n"
-					"mov R$0 R%d", offset, register_index);
-	fputs(			"; Stop getting the value of a variable\n", stream);
-}
-
-static void var_set(Node *root, Table *table, FILE *stream) {
-	char *varname = root->child->data.sval;
-	fputs(			"; Set the value of a variable\n", stream);
-	if(root->child->next->type == WORD_NODE) {
-		if(strcmp(root->child->next->data.sval, "const") == 0) {
-			fprintf(stream, "mov =%d R1\n", root->child->next->child->data.ival);
-		}
-	} else {
-		fprintf(stream, "mov R%d R1\n", root->child->next->data.ival);
-	}
-	fputs(			"mov %0 R0\n"
-					"mul R0 =1024\n"
-					"add R0 =5 R0\n", stream);
-	fprintf(stream, "add R0 =%d R0\n", table_get(table, varname));
-	fputs(			"mov R1 R$0\n"
-					"; End setting the value of a variable\n", stream);
-}
-
-static void node_to_output(Node *root, Table *table, FILE *stream) {
-	char *sval 		= root->data.sval;
-	int ival		= root->data.ival;
-	bool space  	= true;
-	bool eval_child = true;
 	switch(root->type) {
 	case WORD_NODE:
-		if(node_asm_literal(sval, stream)) break;
-		if(strcmp(sval, "const") == 0) {
-			fputc('=', stream);
-			space = false;
-			break;
-		} else if(strcmp(sval, "var-new") == 0) {
-			var_new(root, table, stream);
-			eval_child = false;
+		if(strcmp(sval, "var-new") == 0) {
+			ir_new_var(root->child->data.sval);
+			ir_set_var(root->child->data.sval, root->child->next->data, root->child->next->type);
 		} else if(strcmp(sval, "var-get") == 0) {
-			var_get(root, table, stream);
-			eval_child = false;
+			ir_get_var(root->child->data.sval, root->child->next->data.ival);
 		} else if(strcmp(sval, "var-set") == 0) {
-			var_set(root, table, stream);
-			eval_child = false;
+			ir_set_var(root->child->data.sval, root->child->next->data, root->child->next->type);
 		} else if(strcmp(sval, "defn") == 0) {
 			Node *name_node = root->child;
 			Node *body_node = root->child->next->next;
 			char *name = name_node->data.sval;
-			func_table_add(functions, name, label);
-			fprintf(stream, "lbl =%d \n; start function %s\n", label, name);
-			label++;
-			eval_child = false;
-			table = table_new(table);
-			while(body_node != NULL) {
-				node_to_output(body_node, table, stream);
-				putc('\n', stream);
-				body_node = body_node->next;
+			Node *parameters = name_node->next;
+			char **param_list;
+			if(parameters->type != NIL_NODE) {
+				char **param_list = malloc(sizeof(char*) * 8);
+				size_t length = 1, capacity = 8;
+				Node *current = name_node->next;
+				param_list[0] = current->data.sval;
+				current = current->child;
+				while(current != NULL) {
+					if(length * 2 >= capacity) {
+						capacity *= 2;
+						param_list = realloc(param_list, sizeof(char*) * capacity);
+					}
+					param_list[length] = current->data.sval;
+					length++;
+					current = current->next;
+				}
+				param_list[length] = NULL;
+			} else {
+				param_list = malloc(sizeof(char*));
+				*param_list = NULL;
 			}
-			putc('\n', stream);
-			func_return(stream);
-			fprintf(stream, "\n; return function %s", name);
-			free(table);
+			ir_start_fun(name, param_list);
+			while(body_node != NULL) {
+				node_to_output(body_node);
+			}
+			ir_end_fun();
 		} else if(strcmp(sval, "call") == 0) {
-			Node *func_name = root->child;
-			fprintf(stream, "\n; call function %s\n", func_name->data.sval);
-			call_func(func_name->data.sval, stream);
-			fprintf(stream, "\n; end function call %s\n", func_name->data.sval);
-			eval_child = false;
+			NodeData *data = malloc(sizeof(NodeData) * 2);
+			NodeType *type = malloc(sizeof(NodeType) * 2);
+			size_t capacity = 2;
+			size_t length = 0;
+			Node *arg_node = root->child->next;
+			while(arg_node != NULL) {
+				if(length * 2 < capacity) {
+					data[length] = arg_node->data;
+					type[length] = arg_node->type;
+					length++;
+					arg_node = arg_node->next;
+				} else {
+					capacity *= 2;
+					data = realloc(data, sizeof(NodeData) * capacity);
+					type = realloc(data, sizeof(NodeType) * capacity);
+				}
+			}
+			ir_call_fun(root->child->data.sval, data, type, length);
 		} else if(strcmp(sval, "if") == 0) {
-			Node *condition_node = root->child;
-			Node *if_body = condition_node->next;
-			Node *else_body = if_body->next;
-			fputs("cmp ", stream);
-			node_to_output(condition_node, table, stream);
-			fputs("=0\n", stream);
-			fprintf(stream, "brneq =%d\n", label);
-			node_to_output(if_body, table, stream);
-			putc('\n', stream);
-			fprintf(stream, "brn =%d\n", label + 1); 
-			fprintf(stream, "lbl =%d\n", label);
-			node_to_output(else_body, table, stream);
-			putc('\n', stream);
-			fprintf(stream, "lbl =%d\n", label + 1);
-			label += 2;
-			eval_child = false;
+			ir_if_start();
+			node_to_output(root->child);
+			ir_if_body_start();
+			node_to_output(root->child->next);
+			ir_else_body_start();
+			node_to_output(root->child->next->next);
+			ir_if_end();
 		} else if(strcmp(sval, "while") == 0) {
-			Node *condition_node = root->child;
-			Node *if_body = condition_node->next;
-			fprintf(stream, "lbl =%d\n", label);
-			fputs("cmp ", stream);
-			node_to_output(condition_node, table, stream);
-			fputs("=0\n", stream);
-			fprintf(stream, "brneq =%d\n", label + 1);
-			node_to_output(if_body, table, stream);
-			putc('\n', stream);
-			fprintf(stream, "brn =%d\n", label);
-			fprintf(stream, "lbl =%d\n", label + 1); 
-			label += 2;
-			eval_child = false;
+			ir_while_start();
+			node_to_output(root->child);
+			ir_while_body_start();
+			node_to_output(root->child->next);
+			ir_while_end();
+		} else if(strcmp(sval, "main") == 0) {
+			ir_start_main();
+			node_to_output(root->child);
+			ir_end_main();
+		} else if(strcmp(sval, "asm") == 0) {
+			ir_inline(root->child->data.sval);
 		} else if(strcmp(sval, "true") == 0) {
-			fputs("=1 ", stream);
+			ir_inline("=1 ");
 		} else if(strcmp(sval, "false") == 0) {
-			fputs("=0 ", stream);
-		} else {
-			fputs(sval, stream);
+			ir_inline("=0 ");
 		}
 		break;
-	case NUMBER_NODE:
-		fprintf(stream, "%d", ival);
 	default:
 		break;
-	}
-	if(space) {
-		fputc(' ', stream);
-	}
-	for(Node *child= root->child; eval_child && child != NULL; child = child->next) {
-		node_output(child, stream);
 	}
 }
 
 void node_output(Node *root, FILE *stream) {
-	if(functions == NULL) {
-		functions = func_table_new();
-		global_table = table_new(NULL);
-	}
-	node_to_output(root, global_table, stream);
+	ir_start_file();
+	node_to_output(root);
+	ir_end_file(stream);
 }
